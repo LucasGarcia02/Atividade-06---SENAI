b6f05f130bb2b4fb8de9dc2361722a35c45e7681
LucasGarcia02 <116729960+LucasGarcia02@users.noreply.github.com>
2023-01-27 15:29:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ let idade = prompt("Qual a sua idade?");
 if (idade >=18 ){
     if (quantidadeDeConvidados < 100){
         listaDeConvidados.push(nome);
-        console.log("Cadastro realizado com sucesso")
+        console.log("Cadastro realizado com sucesso");
+    } else {
+        console.log("Limite de participantes alcançado. Não é mais permitido cadastros");
     }
 
 } else {
-    console.log("Menores de idade não podem ser cadastrados.")
+    console.log("Menores de idade não podem ser cadastrados.");
 }
